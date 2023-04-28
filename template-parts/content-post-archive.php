0ae5fa9d36613b40d2b386e78bcb973ba2ce5e86
Danial Bleile <danbleile@gmail.com>
2023-04-28 14:27:47 -0700

    Bug Fix | Archive | Add archive page title

--- template-parts/content-post-archive.php
+++ template-parts/content-post-archive.php
@@ -1,4 +1,5 @@
 <?php get_template_part( 'template-parts/layout-start', 'post_archive', array( 'template' => 'post_archive' ) ); ?>
+<?php get_template_part( 'template-parts/article-header', 'post_archive', array( 'template' => 'post_archive' ) ); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<?php get_template_part( 'template-parts/format', WSUWP\Theme\WDS\Theme::get_wsu_option( 'template_post_archive', 'format', 'index' ), array( 'template' => 'post_archive' ) ); ?>
 <?php endwhile; endif; ?>
--- template-parts/content-tag.php
+++ template-parts/content-tag.php
@@ -1,4 +1,5 @@
 <?php get_template_part( 'template-parts/layout-start', 'tag', array( 'template' => 'tag' ) ); ?>
+<?php get_template_part( 'template-parts/article-header', 'tag', array( 'template' => 'tag' ) ); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<?php get_template_part( 'template-parts/format', WSUWP\Theme\WDS\Theme::get_template_setting( 'template_tag', 'format', 'index' ), array( 'template' => 'tag' ) ); ?>
 <?php endwhile; endif; ?>
