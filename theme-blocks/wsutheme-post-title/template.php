3c62150d8a0bff0811ae9cf12d6f21308285011c
Dan White <dan.white1@wsu.edu>
2023-12-04 16:40:38 -0800

    feat: Hide heading if empty

--- theme-blocks/wsutheme-post-title/template.php
+++ theme-blocks/wsutheme-post-title/template.php
@@ -1,5 +1,8 @@
-<?php if ( ! empty( $args['tag'] ) ) : ?><<?php echo esc_attr( $args['tag'] ); ?>><?php endif; ?>
-	<?php if ( ! empty( $args['link'] ) ) : ?><a href="<?php the_permalink(); ?>"><?php endif; ?>
-	<?php echo $content; ?>
-	<?php if ( ! empty( $args['link'] ) ) : ?></a><?php endif; ?>
-<?php if ( ! empty( $args['tag'] ) ) : ?></<?php echo esc_attr( $args['tag'] ); ?>><?php endif; ?>
+<?php
+	if ( ! empty( $content ) ) : ?>
+	<?php if ( ! empty( $args['tag'] ) ) : ?><<?php echo esc_attr( $args['tag'] ); ?>><?php endif; ?>
+		<?php if ( ! empty( $args['link'] ) ) : ?><a href="<?php the_permalink(); ?>"><?php endif; ?>
+		<?php echo $content; ?>
+		<?php if ( ! empty( $args['link'] ) ) : ?></a><?php endif; ?>
+	<?php if ( ! empty( $args['tag'] ) ) : ?></<?php echo esc_attr( $args['tag'] ); ?>><?php endif; ?>
+<?php endif; ?>
